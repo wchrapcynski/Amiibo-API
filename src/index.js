d0a0e9519993d07afe68c85697e5720747443615
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-15 10:27:21 -0500

    Installed Cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const parser = require("body-parser");
+
+app.use(cors());
 
 app.use(parser.json());
 app.use(require("./routes/index"));
